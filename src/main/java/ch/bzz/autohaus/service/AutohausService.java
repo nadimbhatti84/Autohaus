77bb7a5275b27c4576b13052cc209dfb6e4964d6
Nadim Bhatti <bhattin@bzz.ch>
2022-05-25 11:40:45 +0200

    new Services for Auto, Autohaus and Hersteller

--- src/main/java/ch/bzz/autohaus/service/AutohausService.java
+++ src/main/java/ch/bzz/autohaus/service/AutohausService.java
@@ -4,15 +4,14 @@ import ch.bzz.autohaus.data.DataHandler;
 import ch.bzz.autohaus.model.Auto;
 import ch.bzz.autohaus.model.Autohaus;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 import static javax.ws.rs.core.MediaType.*;
 
@@ -35,7 +34,7 @@ public class AutohausService {
     @Path("read")
     @GET
     @Produces(APPLICATION_JSON)
-    public Response readAutohaus(@QueryParam("id") String name) throws IllegalArgumentException{
+    public Response readAutohaus(@QueryParam("name") String name) throws IllegalArgumentException{
         if(DataHandler.readAutohausByName(name) != null){
             Autohaus autohaus = DataHandler.readAutohausByName(name);
             Response response = Response
@@ -46,6 +45,80 @@ public class AutohausService {
         } else{
             throw new IllegalArgumentException();
         }
+    }
+
+    /**
+     * deletes an Autohaus identified by its Name
+     * @param name
+     * @return
+     */
+    @DELETE
+    @Path("delete")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteAutohaus(
+            @QueryParam("name") String name
+    ){
+        int httpStatus = 200;
+        if(!DataHandler.deleteAutohauser(name)){
+            httpStatus = 410;
+        }
+        return Response
+                .status(httpStatus)
+                .entity("")
+                .build();
+    }
+
+    /**
+     * inserts a new Autohaus
+     * @param name
+     * @param adresse
+     * @return
+     */
+    @POST
+    @Path("create")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response insertBook(
+            @FormParam("name") String name,
+            @FormParam("adresse") String adresse
+    ) {
+        Autohaus autohaus = new Autohaus();
+        autohaus.setName(name);
+        autohaus.setAdresse(adresse);
+
+        DataHandler.insertAutohaus(autohaus);
+        return Response
+                .status(200)
+                .entity("")
+                .build();
+    }
+
+
+    /**
+     * updates a new Autohaus
+     * @param name
+     * @param adresse
+     * @return
+     */
+    @Path("update")
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateBook(
+            @FormParam("name") String name,
+            @FormParam("adresse") String adresse
+    ){
+        int httpStatus = 200;
+        Autohaus autohaus = DataHandler.readAutohausByName(name);
+        if(autohaus != null){
+            autohaus.setAdresse(adresse);
+            autohaus.setName(name);
+            DataHandler.updateAutohauser();
+        }else{
+            httpStatus = 410;
+        }
+        return Response
+                .status(httpStatus)
+                .entity("")
+                .build();
     }
 }
 
--- src/main/java/ch/bzz/autohaus/service/HerstellerService.java
+++ src/main/java/ch/bzz/autohaus/service/HerstellerService.java
@@ -4,15 +4,14 @@ import ch.bzz.autohaus.data.DataHandler;
 import ch.bzz.autohaus.model.Auto;
 import ch.bzz.autohaus.model.Hersteller;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 import static javax.ws.rs.core.MediaType.*;
 
@@ -46,6 +45,78 @@ public class HerstellerService{
         } else{
             throw new IllegalArgumentException();
         }
+    }
+    /**
+     * deletes a Hersteller identified by its Name
+     * @param name
+     * @return
+     */
+    @DELETE
+    @Path("delete")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteHersteller(
+            @QueryParam("id") String name
+    ){
+        int httpStatus = 200;
+        if(!DataHandler.deleteHersteller(name)){
+            httpStatus = 410;
+        }
+        return Response
+                .status(httpStatus)
+                .entity("")
+                .build();
+    }
+
+    /**
+     * inserts a new Hersteller
+     * @param bezeichnung
+     * @param hauptsitz
+     * @return
+     */
+    @Path("create")
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response insertHersteller(
+            @FormParam("bezeichnung") String bezeichnung,
+            @FormParam("hauptsitz") String hauptsitz
+    ) {
+        Hersteller hersteller = new Hersteller();
+        hersteller.setBezeichnung(bezeichnung);
+        hersteller.setHauptsitz(hauptsitz);
+
+        DataHandler.insertHersteller(hersteller);
+        return Response
+                .status(200)
+                .entity("")
+                .build();
+    }
+
+    /**
+     * updates a new Hersteller
+     * @param bezeichnung
+     * @param hauptsitz
+     * @return
+     */
+    @POST
+    @Path("update")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateHersteller(
+            @FormParam("bezeichnung") String bezeichnung,
+            @FormParam("hauptsitz") String hauptsitz
+    ){
+        int httpStatus = 200;
+        Hersteller hersteller = DataHandler.readHerstellerByBezeichnung(bezeichnung);
+        if(hersteller != null){
+            hersteller.setHauptsitz(hauptsitz);
+            hersteller.setBezeichnung(bezeichnung);
+            DataHandler.updateHersteller();
+        }else{
+            httpStatus = 410;
+        }
+        return Response
+                .status(httpStatus)
+                .entity("")
+                .build();
     }
 }
 
